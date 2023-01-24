cc9e7b4355496f0e54504beccbfa57b61ab4a050
[Students] Edgars Zvirgzds <02DPEZvirgzds@rvt.lv>
2023-01-24 13:52:20 +0200

    added some files

--- main.php
+++ main.php
@@ -1,5 +1,35 @@
-<html>
-    <?php
-    echo 'Training';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main page</title>
+</head>
+<body>
+<?php
+        if(array_key_exists('button1', $_POST)) {
+            button1();
+        }
+        else if(array_key_exists('button2', $_POST)) {
+            button2();
+            // header('Location views/about.php');
+            // die();
+        }
+        function button1() {
+            echo "This is Button1 that is selected";
+        }
+        function button2() {
+            echo "This is Button2 that is selected";
+        }
     ?>
+
+    <form method='post'>
+        <input type='submit' name='button1' value='Click me bitch'></input>
+        <input type='submit' name='button2' value='Click me hoe'></input>
+
+    </form>
+
+    <img src='1.jpg' alt=''>
+</body>
 </html>
